a80602464a3978fe68179a2f213abc632eaceed7
Shahriar Kariman <shahriar.kariman@gmail.com>
2024-06-27 14:55:10 -0300

    I made the 4 attack boards will need to work on how to move them.

--- attack_board.js
+++ attack_board.js
@@ -2,13 +2,13 @@ import * as THREE from 'three'
 import { notation, square } from './square'
 import { attack_board_translate, blackSquareMaterial, squareGeometry, whiteSquareMaterial } from './global'
 
-let up = true //
-let forward_backward = 1 // 1 for forward -1 for backward
-let right_left = 1 // 1 for right -1 for left
+let up_down = true // true for up false for down
+let forward_backward = 1 // true for forward false for backward
+let right_left = 1 // true for right false for left
 
-const loop_condition_helper = (val,sign_dependent_val)=>{
+const loop_condition_helper = (val,dependent_val)=>{
   // this is to help with the for loop
-  if(sign_dependent_val>0){
+  if(dependent_val){
     return val<2
   }
   return val>-2
@@ -22,14 +22,15 @@ class attack_board{
     this.original_owner = original_owner // true for white false for black
 
     forward_backward = f_b
+    let f_factor = forward_backward ? 1 : -1
     right_left = r_l
-    up = u_d
-
+    let r_factor = right_left ? 1 : -1
+    up_down = u_d
     
-    for (let col = 0, isStartLight = this.original_owner; loop_condition_helper(col,right_left); col+=right_left, isStartLight = !isStartLight) {
+    for (let col = 0, isStartLight = this.original_owner; loop_condition_helper(col,right_left); col+=r_factor, isStartLight = !isStartLight) {
       let column = String.fromCharCode(this.phase_square.column.charCodeAt(0)+col)
       let obj
-      for(let r = 0, is_light = isStartLight; loop_condition_helper(r,forward_backward); r+=forward_backward, is_light = !is_light){
+      for(let r = 0, is_light = isStartLight; loop_condition_helper(r,forward_backward); r+=f_factor, is_light = !is_light){
         let temp_r = r+this.phase_square.row
         is_light ?
           obj = new THREE.Mesh(squareGeometry,whiteSquareMaterial)
@@ -40,10 +41,10 @@ class attack_board{
         this.squares.add(s.object)
       }
     }
-    up ?
-      this.squares.position.y+=1.5
+    up_down ?
+      this.squares.position.y += 1.5
       :
-      this.squares.position.y-= 1.5
+      this.squares.position.y -= 1.5
   }
 }
 
